1b1a63f998cd202c5b8051183e9a32343b299445
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-09-25 12:36:13 +0200

    Learing use Postman

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.send("Hello World!");
 }); //  This is a route for the root URL of the server and send  a response to the client
 
@@ -13,6 +13,27 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.send("This is the contact page");
 }); // This  is a route for the contact URL of the server and send a response to the client
+ */
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201);
+}); // This  is a route for the register URL of the server and send a response to the client
+
+app.put("/user/dito", (req, res) => {
+  res.sendStatus(200);
+}); //   This is a route for the user/dito URL of the server and send a response to the put request
+
+app.patch("/user/dito", (req, res) => {
+  res.sendStatus(200);
+}); //   This is a route for the user/dito URL of the server and send a response to the patch  request
+
+app.delete("/user/dito", (req, res) => {
+  res.sendStatus(200);
+});//   This is a route for the root URL of the server and send a response to the client to delet  the user
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
