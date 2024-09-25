e60b855d029e11016d00584fcb5e71f29eb553a7
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-09-25 12:11:51 +0200

    HTTP Requests creaeting

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+}); //  This is a route for the root URL of the server and send  a response to the client
+
+app.get("/about", (req, res) => {
+  res.send("This is the about page");
+}); //  This is a route for the about URL of the server and send a response to the client
+
+app.get("/contact", (req, res) => {
+  res.send("This is the contact page");
+}); // This  is a route for the contact URL of the server and send a response to the client
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
